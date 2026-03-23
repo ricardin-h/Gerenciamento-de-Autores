12c1b054dcadf52ab1d912ecf464df9f1488d06c
ricardin-h <ricardohenriquesilva286@gmail.com>
2026-03-22 23:10:43 -0300

    criação da entidade Autor com mapeamento JPA

---
+++ Autores/src/main/java/com/Autores/Autores/Entity/Autor.java
@@ -0,0 +1,55 @@
+package com.Autores.Autores.Entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity(name ="Autor")
+@Table(name= "tb_autor")
+
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="nome")
+    private String nome;
+    @Column(name="nacionalidade")
+    private String nacionalidade;
+    private LocalDate dataNascimento;
+
+    public Autor(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
