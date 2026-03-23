97f72ee175cb89f3b96e1760a51a62af163ac9d8
Ricardo Henrique Silva de Souza <ricardohenriquesilva286@gmail.com>
2026-03-22 22:28:57 -0300

    initial commit

---
+++ Autores/src/test/java/com/Autores/Autores/AutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Autores.Autores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
